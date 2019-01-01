f5ee0812409df2889f9fbc81d31549ace84c2840
Andreas Opferkuch <s-h-a-d-o-w@users.noreply.github.com>
2019-01-01 20:45:35 +0100

    Only grab thumbnail data for selected hashtag

--- src/components/Posts.jsx
+++ src/components/Posts.jsx
@@ -1,17 +1,20 @@
 import * as React from 'react';
 import {query} from '../lib/graphql';
 
-const THUMBS = `
+const generateHashtagQuery = ((strings) => {
+	return (...values) => {
+		return strings[0] + values[0] + strings[1];
+	}
+})`
 {
   locations {
     locId
-    hashtags {
+    hashtags(name: "${0}") {
       name
       posts {
         postId
-        createdOn
-        username
         thumb
+        createdOn
       }
     }
   }
@@ -20,49 +23,71 @@ const THUMBS = `
 
 export default class extends React.Component {
 	state = {
-
+		isFetching: false
 	};
 
-	componentDidMount() {
-		query(THUMBS)
-		.then(response => response.json())
-		.then(result => this.setState({data: result.data}))
-		.catch(error => this.setState({error}));
+	componentDidUpdate(prevProps) {
+		if(this.props.hashtag !== prevProps.hashtag) {
+			this.setState({
+				isFetching: true,
+				hashtag: undefined,
+				data: undefined
+			});
+
+			query(generateHashtagQuery(this.props.hashtag))
+			.then(response => response.json())
+			.then(result => this.setState({
+				isFetching: false,
+				hashtag: this.props.hashtag,
+				data: result.data
+			}))
+			.catch(error => this.setState({
+				isFetching: false,
+				error
+			}));
+		}
 	}
 
-	renderPosts = (data) => {
-		const {hashtag} = this.props;
-
-		if(hashtag === '' || typeof hashtag === 'undefined')
+	renderPosts = () => {
+		const {data, hashtag} = this.state;
+		if(!data || !hashtag)
 			return null;
 
-		let posts = data.locations[0].hashtags.filter(el => el.name === hashtag)[0].posts;
-
 		let thumbs = [];
-		posts.forEach(post => {
-			thumbs.push(
-				<a target="_blank" rel="noopener noreferrer" href={'https://www.instagram.com/p/' + post.postId}>
-					<img
-						alt={post.postId}
-						src={post.thumb}
-						className="thumbnail"
-					/>
-				</a>
-			)
-		});
+		data
+			.locations[0]
+			.hashtags
+			.find(el => el.name === hashtag)
+			.posts
+			.sort((a, b) => b.createdOn - a.createdOn) // Newest first
+			.forEach(post => {
+				thumbs.push(
+					<a key={post.postId}
+					   target="_blank"
+					   rel="noopener noreferrer"
+					   href={'https://www.instagram.com/p/' + post.postId}
+					>
+						<img
+							alt={post.postId}
+							src={post.thumb}
+							className="thumbnail"
+						/>
+					</a>
+				)
+			});
 
 		return thumbs;
 	};
 
 	render() {
-		const {data, error} = this.state;
+		const {error, isFetching} = this.state;
 		return (
 			<main>
 				{error
 					? error
-					: data
-						? this.renderPosts(data)
-						: "Loading..."
+					: isFetching
+						? "Loading..."
+						: this.renderPosts()
 				}
 			</main>
 		);
